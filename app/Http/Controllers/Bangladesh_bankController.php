d8a98db659acb2cdf7e4d78f854fd9b055d9889b
Md. Rajon Hossain <44647557+rajonhossain@users.noreply.github.com>
2024-03-25 18:02:19 +0600

    sadfasdf

--- app/Http/Controllers/Bangladesh_bankController.php
+++ app/Http/Controllers/Bangladesh_bankController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Fdr_model;
+
+
 
 class Bangladesh_bankController extends Controller
 {
     //
     public function Bangladesh_bankpanal(){        
-        return view('bangladeshbank.bangladeshbank');  
+        $data = Fdr_model::all();
+        return view('bangladeshbank.bangladeshbank', ['data' => $data]);        
     }
 }
---
+++ resources/views/bangladeshbank/leftbar.blade.php
@@ -0,0 +1,10 @@
+  <!-- Sidebar (hidden on mobile) -->
+  <div class="sidebar d-none d-md-block bg-info text-white">
+        <!-- <h3>{{ Auth::user()->name; }}</h3> -->
+        <h3>BD Bank</h3>
+        <a href="" class="text-white">Dashboard</a>
+        <a href="" class="text-white">FDR Manage</a>
+        <a href="" class="text-white">Approve FDR List</a>
+        <a href="" class="text-white">Reject FDR List</a>
+        <a href="" class="text-white">Bank Info</a>
+    </div>
