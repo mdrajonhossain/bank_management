69f21395360e8c40c26856234f23a8460299abc7
Md. Rajon Hossain <44647557+rajonhossain@users.noreply.github.com>
2024-04-07 16:51:34 +0600

    asdfasdf

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -49,13 +49,15 @@ class BankController extends Controller{
         $auth_user_id = Auth::id();;
 
         $data = DB::select("
-            SELECT fdr_models.*, bankdatamodels.*, branchdatamodels.*, users.name, users.email as authemail, users.name as auth_name, users.is_active as auth_status 
+            SELECT fdr_models.*, bankdatamodels.*, branchdatamodels.*, 'users.name as auth_name', users.email as authemail, users.name as auth_name, users.is_active as auth_status 
             FROM fdr_models
             LEFT JOIN bankdatamodels ON fdr_models.aply_bank_id = bankdatamodels.id
             LEFT JOIN branchdatamodels ON fdr_models.aply_branch_id = branchdatamodels.id
             LEFT JOIN users ON branchdatamodels.user_id = users.id 
             WHERE bankdatamodels.user_id = $auth_user_id
         ");
+
+
 
 
         return view('bank.bank', ['data' => $data]);
