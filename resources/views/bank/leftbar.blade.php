358758552249180eaa2dd64df27c2501b9da21ea
Md. Rajon Hossain <44647557+rajonhossain@users.noreply.github.com>
2024-04-07 18:46:30 +0600

    sadfasdf

--- resources/views/bank/leftbar.blade.php
+++ resources/views/bank/leftbar.blade.php
@@ -1,6 +1,6 @@
   <!-- Sidebar (hidden on mobile) -->
   <div class="sidebar d-none d-md-block">
-    <h3>{{ Auth::user()->name }}</h3>
+  <h5 class="text-warning">{{ Auth::user()->name; }}</h5>
     <h3 style="color: #7367F0; font-size: 20px; font-weight: bold; margin-top: 20px; margin-bottom: 40px;">Bank Panel</h3>        
     <a href="{{ url('/bank') }}" style="font-size: 15px; font-weight: 500; text-decoration:none; {{ Request::is('bank') ? 'background-color: #ddd; color: blue;' : '' }}">Dashboard</a>        
     <a href="{{ url('/bank/apli_received') }}" style="font-size: 15px; font-weight: 500; text-decoration:none; {{ Request::is('bank/apli_received') ? 'background-color: #ddd; color: blue;' : '' }}">FDR Manage</a>        
